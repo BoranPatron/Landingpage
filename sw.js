7fb0c5ca0d47a45f19101fa657aa8202116c214d
Stephan Schellworth <s.schellworth@valueon.ch>
2025-08-08 12:25:02 +0200

    bugfix mobile version

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => {});
+const CACHE = 'bw-landing-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/logo.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then((hit) => hit || fetch(req).catch(() => caches.match('/index.html')))
+  );
+});
