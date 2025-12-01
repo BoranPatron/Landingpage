b535455f677b870c5a25d873d8ea751f2fd7730a
Stephan Schellworth <s.schellworth@valueon.ch>
2025-12-01 12:39:56 +0100

    Update environment configuration for buildwise-landingpage to use Node.js and adjust start command

---
+++ server-static.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Content Security Policy Header für Google Ads & Analytics
+// Identisch mit netlify.toml Zeile 31
+const cspHeader = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://www.google.com https://*.google.com https://cdn.tailwindcss.com https://unpkg.com; script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://www.google.com https://*.google.com https://cdn.tailwindcss.com https://unpkg.com; connect-src 'self' https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://www.google.com https://*.google.com https://*.analytics.google.com https://*.gstatic.com https://*.onrender.com; img-src 'self' data: blob: https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://www.google.com https://*.google.com https://*.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com https://*.gstatic.com; frame-src 'self' https://www.googletagmanager.com https://*.googletagmanager.com;";
+
+// Setze CSP-Header für alle Requests
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', cspHeader);
+  next();
+});
+
+// Redirect für favicon.png (Browser sucht automatisch nach /favicon.png)
+app.get('/favicon.png', (req, res) => {
+  res.redirect('/assets/favicon.png');
+});
+
+// Statische Dateien ausliefern
+app.use(express.static(path.join(__dirname), {
+  dotfiles: 'ignore',
+  index: false
+}));
+
+// SPA Routing: Alle nicht-existierenden Pfade zu index.html weiterleiten
+// Wie in netlify.toml Zeile 15-18
+// express.static hat bereits alle existierenden Dateien ausgeliefert,
+// daher liefern wir hier nur noch index.html für alle anderen Routen
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Static server listening on port ${port}`);
+});
+
