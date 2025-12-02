f03e3a8145b52ec2da36c4391078a05e2f085fb2
Stephan Schellworth <s.schellworth@valueon.ch>
2025-12-02 13:45:37 +0100

    Re-enable CSP headers with cache-busting to override Cloudflare cache

--- server-static.js
+++ server-static.js
@@ -7,13 +7,19 @@ const app = express();
 // Identisch mit netlify.toml Zeile 31
 const cspHeader = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://www.google.com https://*.google.com https://www.googleadservices.com https://*.googleadservices.com https://googleads.g.doubleclick.net https://*.doubleclick.net https://tpc.googlesyndication.com https://cdn.tailwindcss.com https://unpkg.com; script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://www.google.com https://*.google.com https://www.googleadservices.com https://*.googleadservices.com https://googleads.g.doubleclick.net https://*.doubleclick.net https://tpc.googlesyndication.com https://cdn.tailwindcss.com https://unpkg.com; connect-src 'self' https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://www.google.com https://*.google.com https://*.analytics.google.com https://*.gstatic.com https://*.onrender.com https://www.googleadservices.com https://*.googleadservices.com https://googleads.g.doubleclick.net https://*.doubleclick.net https://tpc.googlesyndication.com https://www.google.com/pagead/ https://*.google.com/pagead/; img-src 'self' data: blob: https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://www.google.com https://*.google.com https://*.gstatic.com https://www.googleadservices.com https://*.googleadservices.com https://googleads.g.doubleclick.net https://*.doubleclick.net https://tpc.googlesyndication.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com https://*.gstatic.com; frame-src 'self' https://www.googletagmanager.com https://*.googletagmanager.com https://www.googleadservices.com https://*.googleadservices.com https://googleads.g.doubleclick.net https://*.doubleclick.net;";
 
-// CSP-Header entfernt - Cloudflare überschreibt die Header mit falscher CSP
-// Stattdessen wird die CSP über Meta-Tag in index.html gesetzt
-// app.use((req, res, next) => {
-//   res.setHeader('Content-Security-Policy', cspHeader);
-//   res.setHeader('X-Content-Security-Policy', cspHeader);
-//   next();
-// });
+// Setze CSP-Header mit höchster Priorität
+// Wichtig: Diese Middleware muss VOR express.static() kommen
+app.use((req, res, next) => {
+  // Setze CSP-Header - Render.com sollte diese weiterleiten
+  res.setHeader('Content-Security-Policy', cspHeader);
+  // Zusätzliche Header für bessere Kompatibilität
+  res.setHeader('X-Content-Security-Policy', cspHeader);
+  // Verhindere Caching der Header
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
 
 // Test-Route: Zeige die aktuelle CSP-Konfiguration
 app.get('/test-csp', (req, res) => {
